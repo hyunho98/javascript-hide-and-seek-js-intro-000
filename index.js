5dbc5e130cdef94dfcdf83156d992413a9755a27
Alex Lee <hyunho98@gmail.com>
2019-05-24 23:02:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,5 +21,5 @@ function increaseRankBy(n) {
 function deepestChild() {
   var grandNode = document.getElementById("grand-node");
   var children = grandNode.children;
-  return (children[children.length - 1]).textContent;
+  return (children[children.length - 1]);
 }
