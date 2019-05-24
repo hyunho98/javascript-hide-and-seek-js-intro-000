b8d7fedddd23651f2cee3f965ab8e11a97f326ac
Alex Lee <hyunho98@gmail.com>
2019-05-24 22:43:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  const nested = document.getElementById('nested');
+  return nested.getElementsByClassName('target')[0];
+}
+
+function increaseRankBy(n) {
+  const rankedList = document.getElementsByClassName(`ranked-list`);
+  for (var i = 0; i < rankedList.length; i++) {
+    for (var x = 0; x < (rankedList[i].children).length; x++) {
+      (rankedList[i].children[x]).textContent = parseInt((rankedList[i].children[x]).getElementsByTagName(`li`)[x]) + n;
+    }
+  }
+}
